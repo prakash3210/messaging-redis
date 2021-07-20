b277a550575d0d7ef4471670ccc17865bb2bb916
prakashsharma <prakash3210@gmail.com>
2021-07-20 19:00:00 +0530

    Commit initial

---
+++ src/test/java/com/example/messaging/redis/MessagingRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.messaging.redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagingRedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
